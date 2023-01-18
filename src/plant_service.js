3926786f354cf3c1dd25098833b52e23827910ae
Tanya Prado <tanyaprado@gmail.com>
2023-01-17 18:55:37 -0800

    add async to PlantService

--- src/plant_service.js
+++ src/plant_service.js
@@ -5,36 +5,43 @@ const options = {
     'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
   }
 };
-export default class PlantService {  
-  static getPlantInfo(plant) {
-    return fetch(`https://house-plants.p.rapidapi.com/common/${plant}`, options)
-      .then(function(response) {
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`;
-          throw new Error(errorMessage);
-        } else {
-          return response.json();
-        }
-      })
-      .catch(function (error) {
-        return error;
-      });
+
+export default class PlantService {
+  static async getPlantInfo(plant) {
+    try {
+      const response = await fetch(`https://house-plants.p.rapidapi.com/common/?q=${plant}`, options);
+      const jResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jResponse;
+    } catch (error) {
+      return error;
+    }
   }
 }
 
-// export default class PlantService {  
-//   static getPlantInfo(plant) {
-//     return fetch(`https://trefle.io/api/v1/plants/search?token=${process.env.API_KEY}q=${plant}`)
-//       .then(function(response) {
-//         if (!response.ok) {
-//           const errorMessage = `${response.status} ${response.statusText}`;
-//           throw new Error(errorMessage);
-//         } else {
-//           return response.json();
-//         }
-//       })      
-//       .catch(function(error) {
-//         return error;
-//       });
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '44cd67c618mshb7b1598b50085f9p1eede7jsn1362f2657e81',
+//     'X-RapidAPI-Host': 'house-plants2.p.rapidapi.com'
+//   }
+// };
+
+// export default class PlantService {
+//   static async getPlantInfo(plant) {
+//     try {
+//       const response = await fetch(`https://house-plants2.p.rapidapi.com/?q=${plant}`, options);
+//       const jResponse = await response.json();
+//       if (!response.ok) {
+//         const errorMessage = `${response.status} ${response.statusText} ${jResponse.message}`;
+//         throw new Error(errorMessage);
+//       }
+//       return jResponse;
+//     } catch (error) {
+//       return error;
+//     }
 //   }
 // }
