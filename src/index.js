010b644812d17be56606c9634f9976b30202b74b
Tanya Prado <tanyaprado@gmail.com>
2023-01-17 18:54:19 -0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import PlantService from './plant_service.js';
+import PlantService from './plant_service';
 
 //Business Logic
 
-function getPlantInfo(plant) {
-  PlantService.getPlantInfo(plant)
-    .then(function(response) {
-      document.querySelector("#showResponse").innerText = response;
-    }, function(errorMessage) {
-      printError(errorMessage);
-    });
-}
+async function getPlantInfo(plant) {
+  const response = await PlantService.getPlantInfo(plant);
+  // let html ='';
+  response.forEach(element => {
+    const div = document.createElement('div');
+    const image = document.createElement('img');
+    const name = document.createElement('h5');
+    const water = document.createElement('p');
+    const light = document.createElement('p');
+    const show = document.querySelector(`#showResponse`);
+    div.classList = 'card'
+    image.classList = 'card-image'
+    image.src = element.img
+    name.innerText = `${element.common}`
+    // name.innerText = `${element[`Common name`]}`
+    water.innerHTML = `<strong>Watering: </strong>${element.watering}`
+    // water.innerHTML = `<strong>Watering: </strong>${element.Watering}`
+    light.innerHTML = `<strong>Light Needs: </strong>${element.ideallight}`
+    // light.innerHTML = `<strong>Light Needs: </strong>${element[`Light tolered`]}`
 
-function printError(errorMessage) {
-  document.querySelector('#showResponse').innerText = errorMessage;
+    div.appendChild(image),
+      div.appendChild(name),
+      div.appendChild(water),
+      div.appendChild(light),
+      show.appendChild(div)
+  });
 }
 
 function handleFormSubmission(event) {
@@ -24,6 +39,42 @@ function handleFormSubmission(event) {
   getPlantInfo(plant);
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
+
+  //   let htmlSegment = `<div class='plantName> 
+  //                        <h2>${element.Categories} ${element[`Common name (fr.)`]}</h2>
+  //                       </div>
+  //                       <div class="showImg"> <img src='${element.img}'>
+  //                       </div>`;
+  //                       html += htmlSegment;
+  // });
+  // document.querySelector('#showResponse').innerHTML = html;
+  // if (response) {
+  //   printElements(response, plant);
+  // } else {
+  //   printError(response, plant)
+  // }
+// }
+//     .then(function (response) {
+//       let output = ''
+//       response.map(function (plant) {
+//         output += `${plant[`Common name (fr.)`]}`;
+//       })
+//       document.querySelector('#showResponse').innerText = output;
+//     }, function (errorMessage) {
+//       printError(errorMessage);
+//     });
+// }
+
+// function printElements(response, plant) {
+//   let output = response;
+//   console.log(response);
+//   document.querySelector('#showResponse').innerText = `Display results for ${plant}: ${output}`;
+// }
+
+// function printError(errorMessage) {
+//   document.querySelector('#showResponse').innerText = errorMessage;
+// }
+
