f6ec3f066c620d84b686f871156eca56d3831821
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-20 21:06:53 -0800

    use Postman fetch syntax to attempt to access API

--- src/index.js
+++ src/index.js
@@ -3,23 +3,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/styles.css';
 
-async function getPlantId(url = `https://my-api.plantnet.org/v2/identify/all`, payload) {
-  const response = await fetch(url, {
+function getPlantID() {
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Content-Type", "multipart/form-data");
+
+  const formdata = new FormData();
+  formdata.append("images", ["/C:/Users/Schadenfreude/OneDrive/Desktop/epicodus_projects/ThirstyPlant/src/assets/images/succulent.jpg"]);
+  formdata.append("organs", "leaf");
+
+  const requestOptions = {
     method: 'POST',
-    params: {
-      'api-key': `${process.env.PlantNet_API_Key}`
-    },
-    headers: {
-      'include-related-images': 'true',
-      'no-reject': 'true',
-      'lang': 'en',
-      'accept': 'application/json',
-      'Content-Type': 'multipart/form-data' 
-    },
-    body: JSON.stringify(payload)
-  });
-  return response.json();
+    headers: myHeaders,
+    body: formdata,
+    redirect: 'follow'
+  };
+
+  fetch("https://my-api.plantnet.org/v2/identify/all?include-related-images=true&no-reject=true&lang=en&api-key=2b10ueg7nSzYau5fSFuJ4PfQ8e", requestOptions)
+    .then(response => response.text())
+    .then(function(result) {
+      printElements(result);
+    })
+    .catch(function(error) {
+      printError(error);
+    });
 }
+
+// async function getPlantId(url = `https://my-api.plantnet.org/v2/identify/all`, payload) {
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     params: {
+//       'api-key': `${process.env.PlantNet_API_Key}`
+//     },
+//     headers: {
+//       'include-related-images': 'true',
+//       'no-reject': 'true',
+//       'lang': 'en',
+//       'accept': 'application/json',
+//       'Content-Type': 'multipart/form-data' 
+//     },
+//     form: {
+      
+//     }
+//   });
+//   return response.json();
+// }
 
 
 //import GetPlantIdFromImage from './services/Plant_Id_API.js';
@@ -47,6 +75,7 @@ async function getPlantId(url = `https://my-api.plantnet.org/v2/identify/all`, p
 
 function handleFormSubmission(e) {
   e.preventDefault();
+  getPlantID();
   // let file = document.querySelector('#myFile');
   // if (!file.value.length) {
   //   return "Please upload a photo to get your plant ID.";
@@ -58,27 +87,27 @@ function handleFormSubmission(e) {
   // imageDisplay.append(img);
 
   //URL.revokeObjectURL(uri);
-  const payload = {
-    'images': '../assets/images/unknown.jpg',
-    'organs': 'leaf'
-  };
-  const url = `https://my-api.plantnet.org/v2/identify/all`;
+  // const payload = {
+  //   'images':'/C:/Users/Schadenfreude/OneDrive/Desktop/epicodus_projects/ThirstyPlant/src/assets/images/succulent.jpg',
+  //   'organs':'leaf'
+  // };
+  //const url = `https://my-api.plantnet.org/v2/identify/all`;
 
-  getPlantId(url, payload)
-    .then(function(response) {
-      if (isNaN(response.created)) {
-        const errorMessage = `${response.statusCode}: ${response.error}: ${response.message}`;
-        throw new Error(errorMessage);
-      } else {
-        return response;
-      }
-    })
-    .then(function(jsonResponse) {
-      printElements(jsonResponse);
-    })
-    .catch(function(error) {
-      printError(error.message);
-    });
+  // getPlantId(url)
+  //   .then(function(response) {
+  //     if (isNaN(response.created)) {
+  //       const errorMessage = `${response.statusCode}: ${response.error}: ${response.message}`;
+  //       throw new Error(errorMessage);
+  //     } else {
+  //       return response;
+  //     }
+  //   })
+  //   .then(function(jsonResponse) {
+  //     printElements(jsonResponse);
+  //   })
+  //   .catch(function(error) {
+  //     printError(error.message);
+  //   });
 }
 
 function printElements(response) {
