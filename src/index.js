00d0ca69f659d31259c5fba427602c13ad767711
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-22 20:33:54 -0800

    copy content over from html-loader error directory.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+//Ariel_Branch
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/styles.css';
-import './services/Plant_Id_API.js';
-import GetPlantIdFromImage from './services/Plant_Id_API.js';
+import GetPlantIdFromLocalImage from './services/Test_Code.js';
+import GetPlantIdFromURL from './services/Plant_Id_API.js';
 
-async function getPlantId() {
-  const response = await GetPlantIdFromImage.getPlantId();
-  if (response.bestMatch) {
+async function getPlantIdFromURL() {
+  const response = await GetPlantIdFromURL.getPlantId();
+  if (response.query) {
     printElements(response);
   } else {
     printError(response);
@@ -15,22 +16,100 @@ async function getPlantId() {
 }
 
 function printElements(response) {
+  const output = document.querySelector('output');
+  let img = document.createElement("img");
+  img.src = `${response.results[0].images[0].url.m}`;
+  output.appendChild(img);
+
+  
   document.querySelector('#showAPIResponse').innerText = `Your plant ID top match is: ${response.bestMatch}, 
-  Common Names are: ${response.results[0].commonNames},
-  ${response.results[0].images[0].url.m}
-  Other possible matches are: ${response.results[1].images[0].url.m} ${response.results[2].images[0].url.m}`;
+  Common Names are: ${response.results[0].species.commonNames},
+
+  Other possible matches are: ${response.results[2].species.commonNames} ${response.results[2].images[0].url.m} 
+
+  ${response.results[4].species.commonNames} ${response.results[4].images[0].url.m}`;
 }
 
-function printError(response) {
-  document.querySelector('#showErrorResponse').innerText = `There was an error accessing the plant information: ${response.statusCode}: ${response['error-type']}.`;
+function printError(error) {
+  document.querySelector('#showErrorResponse').innerText = `There was an error accessing the plant information: ${error}`;
 }
 
 
 function handleFormSubmission(e) {
   e.preventDefault();
-  getPlantId();
+  getPlantIdFromLocalImage();
+  getPlantIdFromURL();
 }
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener('submit', handleFormSubmission);
 });
+
+
+// let imagesArray = [];
+
+// window.addEventListener("change", function(e) {
+//   e.preventDefault();
+//   let fileInput = document.querySelector('input[type="file"]');
+//   imagesArray.push(fileInput.files[0]);
+//   displayImages();
+// });
+
+
+// function displayImages() {
+//   let output = document.querySelector("#output");
+//   let images = "";
+
+//   imagesArray.forEach((image, index) => {
+//     images += `<div class="image">
+//                 <img src="${URL.createObjectURL(image)}" alt="image">
+//                 <span onclick="${deleteImage(index)})">&times;</span>
+//                 </div>`;
+//   });
+
+//   output.innerHTML = images;
+// }
+
+// function deleteImage(index) {
+//   imagesArray.splice(index, 1);
+//   displayImages();
+// }
+
+async function getPlantIdFromLocalImage() {
+  const response = await GetPlantIdFromLocalImage.GetPlantID();
+  if (response.query) {
+    printElements(response);
+  } else {
+    printError(response);
+  }
+}
+
+// function getPlantID() {
+//   const myHeaders = new Headers();
+//   myHeaders.append("accept", "application/json");
+
+//   const formData = new FormData();
+//   formData.append("images", imagesArray[0]);
+//   formData.append("organs", "leaf");
+
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: myHeaders,
+//     body: formData
+//   };
+
+//   let jResponse = response.json();
+
+//   return fetch(`https://my-api.plantnet.org/v2/identify/all?include-related-images=true&no-reject=true&lang=en&api-key=2b10ueg7nSzYau5fSFuJ4PfQ8e`, requestOptions)
+//     .then(function(response) {
+//       if (!response.ok) {
+//         const errorMessage = `${jResponse.statusCode} ${jResponse.error} ${jResponse.message}`;
+//         throw new Error(errorMessage);
+//       } else {
+//         return jResponse;
+//       }
+//     })
+//     .catch(function(error) {
+//       return error;
+//     });
+// }
--- src/services/Plant_Id_API.js
+++ src/services/Plant_Id_API.js
@@ -1,15 +1,15 @@
-export default class GetPlantIdFromImage {
+//Ariel_Branch
+export default class GetPlantIdFromURL {
   static async getPlantId() {
     try {
-      const response = await fetch(`https://my-api.plantnet.org/v2/identify/all?images=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1407305%2Fpexels-photo-1407305.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D1260%26h%3D750%26dpr%3D1&organs=leaf&include-related-images=true&no-reject=true&lang=en&api-key=2b10ueg7nSzYau5fSFuJ4PfQ8e`, {
-        method: 'GET',
+      const response = await fetch(`https://my-api.plantnet.org/v2/identify/all?images=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1407305%2Fpexels-photo-1407305.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D1260%26h%3D750%26dpr%3D1&organs=leaf&include-related-images=true&no-reject=true&lang=en&api-key=${process.env.PlantNet_API_Key}`, {
         headers: {
           'accept': 'application/json'
         }
       });
       const jResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.statusCode}: ${response.error}: ${jResponse.message}`;
+        const errorMessage = `${jResponse.statusCode}: ${jResponse.error}: ${jResponse.message}.`;
         throw new Error(errorMessage);
       }
       return jResponse;
