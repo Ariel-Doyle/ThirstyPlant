2cabb35fb343d89e570f96e27e6ad25c34ec3a9a
Ariel Doyle <ariel.rose.garrett@gmail.com>
2023-01-23 18:21:15 -0800

    add content for both url and local image request and display of images.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/styles.css';
-import GetPlantIdFromLocalImage from './services/Test_Code.js';
-import GetPlantIdFromURL from './services/PlantNet_API.js';
+import GetPlantIdFromLocalImage from './services/PlantNet_LocalImage_API.js';
+import GetPlantIdFromURL from './services/PlantNet_URL_API.js';
+
+//Business
 
 async function getPlantIdFromURL() {
   const response = await GetPlantIdFromURL.getPlantId();
@@ -15,13 +17,51 @@ async function getPlantIdFromURL() {
   }
 }
 
+async function getPlantIdFromLocalImage() {
+  const response = await GetPlantIdFromLocalImage.GetPlantID(localImagesArray);
+  if (response.query) {
+    printElements(response);
+  } else {
+    printError(response);
+  }
+}
+
+//UI
+
+let localImagesArray = [];
+
+function displayLocalImages() {
+  let imageOutput = document.querySelector("#imageOutput");
+  let images = "";
+
+  localImagesArray.forEach((image, index) => {
+    images += `<div class="image">
+                <img src="${URL.createObjectURL(image)}" alt="image">
+                <span onclick="deleteImage(${index})">&times;</span>
+                </div>`;
+  });
+
+  imageOutput.innerHTML = images;
+}
+
+// function displayUrlImage() {
+//   let urlOutput = document.getElementById('urlOuput');
+//   let img = new Image();
+//   img.src = document.querySelector('#imageUrl').value;
+//   urlOutput.innerHTML = `${img.src}`;
+// }
+
+// function deleteImage(index) {
+//   imagesArray.splice(index, 1);
+//   displayImages();
+// }
+
 function printElements(response) {
-  let output = document.querySelector('output');
+  let output = document.querySelector('#showAPIResponse');
   let img = document.createElement("img");
   img.src = `${response.results[0].images[0].url.m}`;
   output.appendChild(img);
 
-  
   document.querySelector('#showAPIResponse').innerText = `Your plant ID top match is: ${response.bestMatch}, 
   Common Names are: ${response.results[0].species.commonNames},
 
@@ -32,56 +72,39 @@ function printElements(response) {
 
 function printError(error) {
   document.querySelector('#showErrorResponse').innerText = `There was an error accessing the plant information: ${error}`;
-function printError(error) {
-  document.querySelector('#showErrorResponse').innerText = `There was an error accessing the plant information: ${error}`;
 }
 
-
-function handleFormSubmission(e) {
-  e.preventDefault();
-  getPlantIdFromLocalImage();
-  getPlantIdFromURL();
-}
+let imageInput = document.getElementById("imageUrl");
 
 window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener('submit', handleFormSubmission);
+  let formImage = document.getElementById('addLocalImage');
+  
+  formImage.addEventListener("change", function() {
+    let fileInput = document.querySelector('input[type="file"]');
+    localImagesArray.push(fileInput.files[0]);
+    displayLocalImages();
+  });
+
+  let formUrl = document.getElementById('addUrl');
+
+  formUrl.addEventListener("input", function() {
+    let image = document.getElementById("image");
+    if (imageInput.value) {
+      image.src = imageInput.value;
+    }
+  });
+
+  let imageBtn = document.getElementById('imageBtn');
+
+  imageBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    getPlantIdFromLocalImage();
+  });
+
+  let urlBtn = document.getElementById('urlBtn');
+
+  urlBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    getPlantIdFromURL();
+  });
 });
-
-
-// let imagesArray = [];
-
-// window.addEventListener("change", function(e) {
-//   e.preventDefault();
-//   let fileInput = document.querySelector('input[type="file"]');
-//   imagesArray.push(fileInput.files[0]);
-//   displayImages();
-// });
-
-
-// function displayImages() {
-//   let output = document.querySelector("#output");
-//   let images = "";
-
-//   imagesArray.forEach((image, index) => {
-//     images += `<div class="image">
-//                 <img src="${URL.createObjectURL(image)}" alt="image">
-//                 <span onclick="${deleteImage(index)})">&times;</span>
-//                 </div>`;
-//   });
-
-//   output.innerHTML = images;
-// }
-
-// function deleteImage(index) {
-//   imagesArray.splice(index, 1);
-//   displayImages();
-// }
-
-async function getPlantIdFromLocalImage() {
-  const response = await GetPlantIdFromLocalImage.GetPlantID(imagesArray);
-  if (response.query) {
-    printElements(response);
-  } else {
-    printError(response);
-  }
-}
---
+++ src/services/PlantNet_LocalImage_API.js
@@ -0,0 +1,30 @@
+export default class GetPlantIdFromLocalImage {
+  static GetPlantID(localImagesArray) {
+    const myHeaders = new Headers();
+    myHeaders.append("accept", "application/json");
+  
+    const formData = new FormData();
+    formData.append("images", localImagesArray[0]);
+    formData.append("organs", "leaf");
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formData
+    };
+  
+    return fetch(`https://my-api.plantnet.org/v2/identify/all?include-related-images=true&no-reject=true&lang=en&api-key=2b10ueg7nSzYau5fSFuJ4PfQ8e`, requestOptions)
+      .then(function(response) {
+        if (!response.ok) {
+          const jResponse = response.json();
+          const errorMessage = `${jResponse.statusCode} ${jResponse.error} ${jResponse.message}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
