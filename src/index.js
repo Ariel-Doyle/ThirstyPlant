137fb3a1050756b2edf9af3f8b703d5824a66b6c
Tanya Prado <tanyaprado@gmail.com>
2023-01-23 20:58:53 -0800

    fix bug collecting plant data

--- src/index.js
+++ src/index.js
@@ -2,53 +2,64 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import PlantService from './plant_service';
-import TimerService from './timer_service';
-
-// Business Logic
 
 async function getPlantInfo(plant) {
-  const response = await PlantService.getPlantInfo(plant);
-  const div = document.createElement('div');
-  const image = document.createElement('img');
-  const name = document.createElement('h5');
-  const water = document.createElement('p');
-  const light = document.createElement('p');
-  const show = document.querySelector(`#showResponse`);
-  div.classList = 'card';
-  image.classList = 'card-image';
-  name.innerText = response[0].common;
-  water.innerHTML = `<strong>Watering: </strong>${response[0].watering}`;
-  light.innerHTML = `<strong>Light Needs: </strong>${response[0].ideallight}`;
+  const response = await PlantService.getPlantInfo();
+  if (response.toString().includes("Error")) {
+    printError(response, plant);
+  } else {
+    printElements(response, plant);
+  }
+}
 
-  div.appendChild(image);
-  div.appendChild(name);
-  div.appendChild(water);
-  div.appendChild(light);
-  show.appendChild(div);
+function printElements(response, plant) {
+  document.querySelector('#showResponse').innerHTML = "";
+  console.log(plant);
+  console.log(response[0]['Common name'][0]);
+  let thisArray = (response.filter(function (element) {
+    if (element['Common name'] === null) {
+      console.log('null');
+    } else if (element['Common name'][0] === plant) {
+      console.log(element);
+      return element;
+    }
+  }));
+  thisArray.forEach(element => {
+    const div = document.createElement('div');
+    const image = document.createElement('img');
+    const name = document.createElement('h5');
+    const water = document.createElement('p');
+    const light = document.createElement('p');
+    const show = document.querySelector('#showResponse');
+    div.classList = 'card';
+    image.classList = 'card-image';
+    image.src = element.img;
+    name.innerHTML = `<strong>${element['Common name']}</strong> \n ${element['Latin name']}`;
+    water.innerHTML = `<strong>Watering: </strong>${element.Watering}`;
+    light.innerHTML = `<strong>Light Needs: </strong>${element['Light tolered']}`;
+
+    div.appendChild(image),
+    div.appendChild(name),
+    div.appendChild(water),
+    div.appendChild(light),
+    show.appendChild(div);
+    document.querySelector('#plant').value = null;
+  });
+}
+
+function printError(errorMessage) {
+  document.querySelector('#showResponse').innerText = errorMessage;
 }
 
 function handleFormSubmission(event) {
-  let plant = document.querySelector('#plant').value;
   event.preventDefault();
+  let plant = document.querySelector('#plant').value;
   getPlantInfo(plant);
 }
 
 window.addEventListener("load", function () {
   document.querySelector("form").addEventListener("submit", handleFormSubmission);
-  document.querySelector("reminder-form").addEventListener("submit", createReminder);
 });
 
 
-// UI Logic
 
-function reminderMsg(days) {
-  let msg = `Reminder to water your plants! You last watered ${days} days ago.`;
-  //return the message in a modal popup box using css
-  return msg;
-}
-
-function createReminder() {
-  let days = document.querySelector("#days").value;
-  let time = days * 86400000;
-  setTimeout(reminderMsg(days), time);
-}
